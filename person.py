d98969b5eb615aeb495f850165f68f8c0a554351
Andrey Tkachev <tka4.andr@yandex.ru>
2018-02-23 16:31:53 +0300

    person merge

--- person.py
+++ person.py
@@ -8,11 +8,13 @@ class Person(object):
     income_info = [] # [(year, income)]
     relative_info = [] # [(year, user_id)]
     region_info = [] # [(year, id)]
-    real_estates = [] # [(year, id)]
+    real_estates = [{ 'year' : 0,
+                     'reg_id' : 0,
+                     'type_id' : 0,
+                     'square' : 0.0}]
     vehicles_info = [] # [(year, brand_id)]
     def __init__(self):
         pass
-
 
 def person_factory(mongo_dict):
     return Person()
