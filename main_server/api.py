dc434bf05fc1177e3bf01e60345994943fa1b2a1
tavplubix <tavplubix@gmail.com>
2018-02-24 18:13:04 +0300

    sabarsrhens

--- main_server/api.py
+++ main_server/api.py
@@ -16,6 +16,43 @@ db = pymongo.MongoClient('mongodb://35.229.26.187:2700/declarator').declarator
 en = Engine(db, db, gc)
 
 
+html = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>StackCarruption</title>
+</head>
+<body>
+
+<form action="http://35.204.32.159:8080" method="get">
+   <p>
+       <select name="method">
+            <option value="get_person_id">ID по ФИО</option>
+            <option  value="get_neighbourgs">список ID соседей по графу по ID</option>
+            <option  value="get_connection">получить иформацию о связи arg1 и arg2</option>
+            <option  value="add_connection">добавить информацию arg3 о связи arg1 и arg2</option>
+            <option  value="confirm_connection">подтвердить связь № arg1 между arg2 и arg3</option>
+            <option  value="decline_connection">опровергнуть связь № arg1 между arg2 и arg3</option>
+
+       </select>
+    <p>
+    <b>Arg1:</b><br>
+    <input name="arg1" type="text" size="40">
+    </p>
+    <p><b>Arg2:</b><br>
+    <input name="arg2" type="text" size="40">
+    </p>
+    <p><b>Arg2:</b><br>
+    <input name="arg3" type="text" size="40">
+    </p>
+</p>
+   <p><input type="submit" value="Отправить"></p>
+</form>"""
+html_end = """
+</body>
+</html>"""
+
+
 def build_connection(server_adress: str, database: str):
     client = pymongo.MongoClient(server_adress)
     return client[database]
@@ -54,7 +91,7 @@ class ApiHandler(tornado.web.RequestHandler):
     def get(self):
         request_method = self.get_query_argument("method")
         result = perform_action(request_method, *extract_parameters(self))
-        self.write(result)
+        self.write(html + str(result) + html_end)
         self.flush()
 
 
